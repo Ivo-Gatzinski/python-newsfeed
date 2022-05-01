0573f542d0d6c9ce5769777ccf83847e96b93064
Ezhko <ucsd.camp.21@gmail.com>
2022-04-30 17:23:24 -0700

    edit api

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -6,19 +6,27 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
- return render_template(
+
+  db = get_db()  
+  posts = (
+    db.query(Post)
+    .filter(Post.user_id == session.get('user_id'))
+    .order_by(Post.created_at.desc())
+    .all()
+  )
+  return render_template(
   'dashboard.html',
   posts=posts,
   loggedIn=session.get('loggedIn')
   )
-db = get_db()  
-posts = (
-  db.query(Post)
-  .filter(Post.user_id == session.get('user_id'))
-  .order_by(Post.created_at.desc())
-  .all()
-  )
 
 @bp.route('/edit/<id>')
 def edit(id):
-  return render_template('edit-post.html')
+  db = get_db()
+  post = db.query(Post).filter(Post.id == id).one()
+
+  return render_template(
+    'edit-post.html',
+    post=post,
+    loggedIn=session.get('loggedIn')
+  )
